2148791fe4db989d28e02d63e8cb619e17b1bca4
DennisMartel <58192704+DennisMartel@users.noreply.github.com>
2021-02-08 13:37:09 -0600

    Update panel.blade.php

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -366,27 +366,11 @@
 <script>
   $(function () {
     $("#example1").DataTable({
+    "language": {
+        "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+    },
       "responsive": true,
       "autoWidth": false,
-       language: {
-          "decimal": "",
-          "emptyTable": "No hay información",
-          "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-          "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-          "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-          "infoPostFix": "",
-          "thousands": ",",
-          "lengthMenu": "Mostrar _MENU_ Entradas",
-          "loadingRecords": "Cargando...",
-          "processing": "Procesando...",
-          "search": "Buscar:",
-          "zeroRecords": "No encontramos ningun registro",
-          "paginate": {
-              "first": "Primero",
-              "last": "Ultimo",
-              "next": "Siguiente",
-              "previous": "Anterior"
-          }
     });
 
     bsCustomFileInput.init();
