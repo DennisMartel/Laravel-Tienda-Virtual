7bbb475782c8e0f30ecc7ad2eb04face574a2353
DennisMartel <58192704+DennisMartel@users.noreply.github.com>
2021-02-08 09:29:54 -0600

    Update panel.blade.php

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -368,6 +368,25 @@
     $("#example1").DataTable({
       "responsive": true,
       "autoWidth": false,
+       language: {
+          "decimal": "",
+          "emptyTable": "No hay información",
+          "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+          "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+          "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+          "infoPostFix": "",
+          "thousands": ",",
+          "lengthMenu": "Mostrar _MENU_ Entradas",
+          "loadingRecords": "Cargando...",
+          "processing": "Procesando...",
+          "search": "Buscar:",
+          "zeroRecords": "No encontramos ningun registro",
+          "paginate": {
+              "first": "Primero",
+              "last": "Ultimo",
+              "next": "Siguiente",
+              "previous": "Anterior"
+          }
     });
 
     bsCustomFileInput.init();
