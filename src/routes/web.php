7c705d7292acb1ce8b125787ad230dace2ffc9a3
DennisMartel <marteldennis05@gmail.com>
2021-03-05 16:42:49 -0600

    rutas

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,21 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('home');
-});
-
 Auth::routes();
 
 // User Web Routes
@@ -28,6 +13,7 @@ Route::get('/{departamento}', [App\Http\Controllers\HomeController::class, 'depa
 Route::get('/{categoria}', [App\Http\Controllers\HomeController::class, 'categoria'])->name('categoria');
 Route::get('/{subcategoria}', [App\Http\Controllers\HomeController::class, 'subcategoria'])->name('subcategoria');
 Route::get('/producto/{producto}', [App\Http\Controllers\HomeController::class, 'producto'])->name('producto');
+Route::get('/checkout', [App\Http\Controllers\HomeController::class, 'checkout'])->name('checkout');
 
 // Admin Routes
 Route::group(['middleware' => ['role:admin']], function() {
